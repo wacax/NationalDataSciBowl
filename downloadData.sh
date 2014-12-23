8ccd8f18d3a7f694195da5c75995a3007cca7316
wacax <wacax@hotmail.com>
2014-12-23 18:48:30 -0500

    added files and merging local files with origin

---
+++ downloadData.sh
@@ -0,0 +1,5 @@
+wget -x --load-cookies cookies.txt -P Data -nH --cut-dirs=5 http://www.kaggle.com/c/datasciencebowl/download/sampleSubmission.csv.zip
+wget -x --load-cookies cookies.txt -P Data -nH --cut-dirs=5 http://www.kaggle.com/c/datasciencebowl/download/train.zip
+wget -x --load-cookies cookies.txt -P Data -nH --cut-dirs=5 http://www.kaggle.com/c/datasciencebowl/download/test.zip
+wget -x --load-cookies cookies.txt -P Data -nH --cut-dirs=5 http://www.kaggle.com/c/datasciencebowl/download/plankton_identification.pdf
+
